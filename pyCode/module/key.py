7bfeaea99ed6bb665144bf5fd4a5c6beb96ab910
eiji-noguchi <eiji.program@gmail.com>
2019-02-17 03:41:33 +0900

    Telloをキー入力で操作できるようにした

---
+++ pyCode/module/key.py
@@ -0,0 +1,26 @@
+from msvcrt import getch
+
+def getCommand(getch):
+    key = chr(ord(getch))
+    if key == "\r":
+        return "takeoff"
+    elif key == " ":
+        return "land"
+    elif key == "q":
+        return "up 20"
+    elif key == "e":
+        return "down 20"
+    elif key == "w":
+        return "forward 20"
+    elif key == "a":
+        return "left 20"
+    elif key == "s":
+        return "back 20"
+    elif key == "d":
+        return "right 20"
+    elif key == "b":
+        return "battery?"
+    elif key == "c":
+        return input("コマンドを入力してね( *´艸｀)\r\n")
+    else:
+        return key
---
+++ pyCode/module/test.py
@@ -0,0 +1,15 @@
+#import json
+def getJson():
+    return json
+    
+json = {
+    "takeoff":"takeoff",
+    "land":"land",
+    "q": "up 20",
+    "e": "down 20",
+    "w": "forward 20",
+    "a": "left 20",
+    "s": "back 20",
+    "d": "right 20",
+    "b": "battery?"
+}
