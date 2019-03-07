8e6d89c26e9e7fb0da3ca0942de6f7e95472b340
eiji <nogunogueiji@gmail.com>
2019-03-07 10:23:00 +0900

    試し書き用ファイル

--- Sample/sample.py
+++ Sample/sample.py
@@ -1,3 +1,4 @@
+"""
 print("hello")
 st = "hello"
 data = b"hello"
@@ -10,3 +11,17 @@ print(data.decode())
 inp = input()
 print(type(inp))
 print(type(inp.encode()))
+"""
+
+
+from msvcrt import getch
+msg = chr(ord(getch()))
+print(msg)
+
+"""
+def f(x=2):
+    return x ** x
+
+print(f())
+print(f(4))
+"""
